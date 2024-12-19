1175cfba1027c98b2b38552c78ab3f066f1dfa57
kristinaSumarok <ktroitski33@gmail.com>
2024-12-19 18:40:35 +0200

    dialog panel refactored, speaker name added

--- Assets/Scripts/Dialogue/DIalogueManager.cs
+++ Assets/Scripts/Dialogue/DIalogueManager.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 using Ink.Runtime;
+using System.Collections;
+using System.Collections.Generic;
 
 public class DialogueManager: MonoBehaviour
 {
     [Header("Dialogue UI")]
     [SerializeField] private GameObject panel;
     [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private TextMeshProUGUI displatNameText;
     private Story currentStory;
     private bool dialogueIsPlaying;
     private static DialogueManager instance;
-
+    private const string SPEAKER_TAG = "speaker";
     private void Awake(){
         if(instance != null){
             Debug.LogWarning("Found more than one dialogue manager in the scene");
@@ -41,11 +45,15 @@ public class DialogueManager: MonoBehaviour
         currentStory = new Story(inkJSON.text);
         dialogueIsPlaying = true;
         panel.SetActive(true);
-
+        currentStory.BindExternalFunction("loadScene", () => {
+         SceneManager.LoadSceneAsync(2);
+        });
         ContinueStory();
     }
 
-    private void ExitDialogueMode(){
+    private IEnumerator ExitDialogueMode(){
+        yield return new WaitForSeconds(0.2f);
+
         dialogueIsPlaying = false;
         panel.SetActive(false);
         text.text = "";
@@ -54,9 +62,23 @@ public class DialogueManager: MonoBehaviour
     private void ContinueStory(){
         if (currentStory.canContinue){
             text.text = currentStory.Continue();
+            HandleTags(currentStory.currentTags);
         }
         else{
-            ExitDialogueMode();
+            StartCoroutine(ExitDialogueMode());
+        }
+    }
+    private void HandleTags(List<string> currentTags){
+        foreach (string tag in currentTags){
+            string[] splitTag = tag.Split(':');
+            if (splitTag.Length !=2){
+                Debug.LogError("Tag could not be appropriately parsed: " + tag);
+            }
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            displatNameText.text = tagValue;
+
         }
     }
 
